a8bc94009f4f2e4269612af705d0da29062908e3
Javier <jeguerrerocordoba@gmail.com>
2024-04-02 07:38:00 -0500

    Serializers added

---
+++ LittleLemonAPI/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import *
+from rest_framework.validators import UniqueTogetherValidator
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'slug', 'title')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Category.objects.all(),
+                fields=('slug','title'),
+            ),
+        ]
+        
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    category_id = serializers.IntegerField(write_only=True)
+    
+    class Meta:
+        model = MenuItem
+        fields = ('id', 'title', 'price', 'featured', 'category', 'category_id')
+        
+class CartSerializer(serializers.ModelSerializer):
+    menuitem = MenuItemSerializer(read_only=True)
+    menuitem_id = serializers.IntegerField(write_only=True)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    
+    class Meta:
+        model = Cart
+        fields = ('id', 'user', 'menuitem', 'menuitem_id', 'quantity', 'unit_price', 'price')
+        
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    
+    class Meta:
+        model = Order
+        fields = ('id', 'user', 'delivery_crew', 'status', 'total', 'date')
+        
+class OrderItemSerializer(serializers.ModelSerializer):
+    order = OrderSerializer(read_only=True)
+    order_id = serializers.IntegerField(write_only=True)
+    menuitem = MenuItemSerializer(read_only=True)
+    menuitem_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'order', 'order_id', 'menuitem', 'menuitem_id', 'quantity', 'unit_price', 'price')
