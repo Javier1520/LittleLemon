a681efb8463f8cd61c4106f3c7fc078480568f19
Javier <jeguerrerocordoba@gmail.com>
2024-04-02 05:44:34 -0500

    djoser, authtoken and urls modififed, migrations applied

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     
     # third party
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     
     #
     'LittleLemonAPI',
@@ -127,3 +129,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
+}
