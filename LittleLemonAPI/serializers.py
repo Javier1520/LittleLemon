c590173e4d04a0d08b8c8a029d01ae5cfed65afd
Javier Guerrero <jeguerrerocordoba@gmail.com>
2024-04-04 02:41:12 -0500

    cart only accepts menuitem_id and quantity

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -12,37 +12,60 @@ class CategorySerializer(serializers.ModelSerializer):
                 fields=('slug','title'),
             ),
         ]
-        
+
 class MenuItemSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
-    
+
     class Meta:
         model = MenuItem
         fields = ('id', 'title', 'price', 'featured', 'category', 'category_id')
-        
+
 class CartSerializer(serializers.ModelSerializer):
     menuitem = MenuItemSerializer(read_only=True)
     menuitem_id = serializers.IntegerField(write_only=True)
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2, read_only=True)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, read_only=True)
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    
+
     class Meta:
         model = Cart
         fields = ('id', 'user', 'menuitem', 'menuitem_id', 'quantity', 'unit_price', 'price')
-        
+
+    def create(self, validated_data):
+        menuitem_id = validated_data.pop('menuitem_id')
+        quantity = validated_data.get('quantity')
+
+        try:
+            menuitem = MenuItem.objects.get(pk=menuitem_id)
+        except MenuItem.DoesNotExist:
+            raise serializers.ValidationError("Invalid MenuItem primary key.")
+
+        validated_data['menuitem'] = menuitem
+        validated_data['unit_price'] = menuitem.price
+        validated_data['price'] = menuitem.price * quantity
+        return super().create(validated_data)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['unit_price'] = instance.unit_price
+        representation['price'] = instance.price
+        return representation
+
 class OrderSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    user_id = serializers.IntegerField(read_only=True)  
+    user_id = serializers.IntegerField(read_only=True)
     total = serializers.DecimalField(max_digits=6, decimal_places=2, read_only=True)
     status = serializers.BooleanField(read_only=True)
     delivery_crew = serializers.ReadOnlyField(source='delivery_crew.username')
     delivery_crew_id = serializers.IntegerField(read_only=True)
     date = serializers.DateTimeField(read_only=True, format="%d/%m/%Y - %H:%M:%S")
-    
+    order_items = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Order
-        fields = ('id', 'user','user_id', 'delivery_crew','delivery_crew_id', 'status', 'total', 'date')        
-        
+        fields = ('id', 'user','user_id', 'delivery_crew','delivery_crew_id', 'status', 'total', 'date', 'order_items')
+
 class OrderItemSerializer(serializers.ModelSerializer):
     order = OrderSerializer(read_only=True)
     order_id = serializers.IntegerField(write_only=True)
@@ -52,7 +75,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ('id', 'order', 'order_id', 'menuitem', 'menuitem_id', 'quantity', 'unit_price', 'price')
-        
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 from django.contrib.auth.models import User, Group
 from .models import *
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from .serializers import *
 from rest_framework import status
@@ -19,7 +19,7 @@ class CategoryView(generics.ListCreateAPIView):
             return Response({'message': 'Only managers can create categories'}, status=status.HTTP_403_FORBIDDEN)
 
         return super().create(request, *args, **kwargs)
-    
+
 class SingleCategoryView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -36,19 +36,19 @@ class SingleCategoryView(generics.RetrieveUpdateDestroyAPIView):
             return Response({'message': 'Only managers can delete categories'}, status=status.HTTP_403_FORBIDDEN)
 
         return super().destroy(request, *args, **kwargs)
-        
+
 # MenuItem views
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     permission_classes = (IsAuthenticated,)
-    
+
     def create(self, request, *args, **kwargs):
         if not request.user.groups.filter(name='Manager').exists():
             return Response({'message': 'Only managers can create menu items'}, status=status.HTTP_403_FORBIDDEN)
-            
+
         return super().create(request, *args, **kwargs)
-    
+
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
@@ -65,18 +65,18 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
             return Response({'message': 'Only managers can delete menu items'}, status=status.HTTP_403_FORBIDDEN)
 
         return super().destroy(request, *args, **kwargs)
-    
+
 # User group managent
 class ManagerView(generics.ListCreateAPIView):
     queryset = User.objects.filter(groups__name='Manager')
     serializer_class = GroupSerializer
     permission_classes = (IsAuthenticated,)
-    
+
     def get(self, request, *args, **kwargs):
         if not request.user.groups.filter(name='Manager').exists():
             return Response({'message': 'Only managers can access'}, status=status.HTTP_403_FORBIDDEN)
         return super().get(request, *args, **kwargs)
-    
+
     def create(self, request, *args, **kwargs):
         if not request.user.groups.filter(name='Manager').exists():
             return Response({'message': 'Only managers can access'}, status=status.HTTP_403_FORBIDDEN)
@@ -84,7 +84,7 @@ class ManagerView(generics.ListCreateAPIView):
         user = get_object_or_404(User, username=request.data.get('username'))
         manager_group.user_set.add(user)
         return Response({'message': 'User added to manager group'})
-    
+
 class SingleManagerView(generics.DestroyAPIView):
     queryset = User.objects.filter(groups__name='Manager')
     serializer_class = GroupSerializer
@@ -115,7 +115,7 @@ class DeliveryCrewView(generics.ListCreateAPIView):
         user = get_object_or_404(User, username=request.data.get('username'))
         delivery_crew_group.user_set.add(user)
         return Response({'message': 'User added to delivery crew group'})
-    
+
 class SingleDeliveryCrewView(generics.DestroyAPIView):
     queryset = User.objects.filter(groups__name='Delivery crew')
     serializer_class = GroupSerializer
@@ -128,48 +128,48 @@ class SingleDeliveryCrewView(generics.DestroyAPIView):
         user = get_object_or_404(User, pk=pk)
         delivery_crew_group.user_set.remove(user)
         return Response({'message': 'User removed from delivery crew group'})
-    
+
 class CartView(generics.ListCreateAPIView):
     serializer_class = CartSerializer
     permission_classes = (IsAuthenticated,)
-    
+
     def get_queryset(self):
         user = self.request.user
         return Cart.objects.filter(user=user)
-    
+
     def create(self, request, *args, **kwargs):
         try:
             return super().create(request, *args, **kwargs)
         except IntegrityError:
             return Response({'message': 'Item already in cart'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
     def delete(self, request, *args, **kwargs):
         user = self.request.user
         cart = Cart.objects.filter(user=user)
         cart.delete()
         return Response({'message': 'Cart cleared'})
-    
-class OrderView(generics.ListCreateAPIView):    
+
+class OrderView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
-    
-    def get(self, request, *args, **kwargs):        
-        if request.user.groups.filter(name='Manager').exists():                     
+
+    def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Manager').exists():
             return super().get(request, *args, **kwargs)
         elif request.user.groups.filter(name='Delivery crew').exists():
             orders_to_deliver = Order.objects.filter(delivery_crew=request.user)
             serializer = OrderSerializer(orders_to_deliver, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        else:            
-            customer_orders = Order.objects.filter(user=request.user)            
-            serializer = OrderSerializer(customer_orders, many=True)            
+        else:
+            customer_orders = Order.objects.filter(user=request.user)
+            serializer = OrderSerializer(customer_orders, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def perform_create(self, serializer):               
+
+    def perform_create(self, serializer):
         carts = Cart.objects.filter(user=self.request.user)
-        total = sum(cart.price for cart in carts)        
-        
+        total = sum(cart.price for cart in carts)
+
         order = serializer.save(total=total, user=self.request.user)
         for cart in carts:
             order_item = OrderItem.objects.create(
@@ -178,21 +178,21 @@ class OrderView(generics.ListCreateAPIView):
                 quantity=cart.quantity,
                 unit_price=cart.unit_price,
                 price=cart.price
-            )            
-            cart.delete()        
-        
+            )
+            cart.delete()
+
         return serializer.instance
-    
+
 class SingleOrderView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
-    
+
     def get(self, request, pk, *args, **kwargs):
         if request.user.groups.filter(name='Manager').exists():
             exac_order = Order.objects.get(pk=pk)
             serializer = OrderSerializer(exac_order)
-            return Response(serializer.data, status=status.HTTP_200_OK)            
+            return Response(serializer.data, status=status.HTTP_200_OK)
         elif request.user.groups.filter(name='Delivery crew').exists():
             orders_to_deliver = Order.objects.filter(delivery_crew=request.user, pk=pk)
             serializer = OrderSerializer(orders_to_deliver, many=True)
