7bfc56fe78e7d3393135b9e1e8b2d38c4b71c4a8
Javier Guerrero <jeguerrerocordoba@gmail.com>
2024-06-08 21:55:36 -0500

    manage.py bug solved

--- littlelemon/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from restaurant.models import Menu
-
-class MenuItemTest(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(title='Chocolate', price=50, inventory=100)
-        self.assertEqual(item.title, "Chocolate")
-        self.assertEqual(item.price, 50)
